e82d537880545ef904ce85ffea9eeeb16771cd83
Anuj Gunjad <46643027+anujgunjad@users.noreply.github.com>
2023-02-25 12:58:12 +0530

    state build

--- React/src/components/Body.js
+++ React/src/components/Body.js
@@ -1,12 +1,31 @@
 import { restaurantList } from "../contants";
 import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
+
+function filterData(txt, data) {
+    const result = data.filter((dataItem) => 
+      dataItem.data.name.includes(txt)
+    );
+    console.log(result);
+    return result;
+} 
+
 const Body = () => {
-    const [searchTxt] = useState(); //This function return 
+    const [searchTxt, setSearchTxt] = useState(""); //This function return 
+    const [restaurantData, setRestaurantData] = useState(restaurantList);
+
     return (
         <>
+        <div className="search-input-wrap">
+            <input type="text" placeholder="search items" value={searchTxt} onChange={(e) => setSearchTxt(e.target.value)} />
+            <button type="button" onClick={() => {
+                let data = filterData(searchTxt, restaurantData);
+                setRestaurantData(data);
+            }   
+            }>Search</button>
+        </div>
         <div className='restaurant-list'>
-            {restaurantList.map((restaurant) => {
+            {restaurantData.map((restaurant) => {
                 return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
             })}
         </div>
